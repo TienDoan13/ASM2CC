dae3b901ace25f6ba0115d43f849a49b961b9cc0
TienDoan13 <105089710+TienDoan13@users.noreply.github.com>
2022-06-10 08:17:02 +0700

    git init

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 ﻿<!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>double T Toys </title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
     <style type="text/css">
-    	.navbar {
+        .navbar {
             background-color: #242e4c;
         }
 
@@ -51,11 +52,152 @@
         margin-left: 20px;
         margin-top: 5px;
         }
+        .search {
+        height: 40px;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        background: #2f3640;
+        border-radius: 50px;
+        padding: 10px;
+        }
+        .sbutton {
+        color: #e84118;
+        float: right;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        background: #DADADA;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-decoration: none;
+        transition: 0.4s;
+        }
+        .stext {
+        border: none;
+        background: none;
+        outline: none;
+        float: left;
+        padding: 0;
+        color: #555;
+        font-size: 15px;
+        transition: 0.4s;
+        line-height: 40px;
+        width: 0px;
+        }
+        .sbox:hover > .stext {
+        width: 200px;
+        padding: 0 5px;
+        border-radius: 50px;
+        }
+        .sbox:hover > .sbutton {
+        background: #fff;
+        }
+        .fa-search {
+        color: #001EFF;
+        }
     </style>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">Kingdom for kids</a>
+            <a class="navbar-brand" href="index.php">double T Toys - Kingdom for kids</a>
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown   ml-lg-3 ">
+                        <a class="nav-link dropdown-toggle text-light d-inline-block rounded p-1 p-lg-2" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
+                            Toys
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-left p-3" aria-labelledby="navbarDropdownMenuLink">
+                            <a href=""style="text-decoration: none;">Pokemon</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Jurassic World</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Wrestling</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Fortnite</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Barbie</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">LEGO</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Jigsaws & Puzzles</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Books</a>
+                        </ul>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown   ml-lg-3 ">
+                        <a class="nav-link dropdown-toggle text-light d-inline-block rounded p-1 p-lg-2" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
+                            Baby Room
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-left p-3" aria-labelledby="navbarDropdownMenuLink">
+                            <a href=""style="text-decoration: none;">Car Seats</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Travel Systems</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Potty Training</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Nursery & Bed</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Baby Monitors</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Jumpers</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Playmats</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Bouncers</a>
+                        </ul>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown   ml-lg-3 ">
+                        <a class="nav-link dropdown-toggle text-light d-inline-block rounded p-1 p-lg-2" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
+                            Outdoor
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-left p-3" aria-labelledby="navbarDropdownMenuLink">
+                            <a href="" style="text-decoration: none;">Trampolines</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Swings & Slides</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Sand & Water</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Play Houses</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Bikes</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Scooters</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Trikes</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Goals</a>
+                        </ul>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown   ml-lg-3 ">
+                        <a class="nav-link dropdown-toggle text-light d-inline-block rounded p-1 p-lg-2" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
+                            Gaming
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-left p-3" aria-labelledby="navbarDropdownMenuLink">
+                            <a href=""style="text-decoration: none;">Xbox Gaming</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Playstation</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Nintendo</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Pre-Oder Games</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Smart Watches</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Gaming Chairs</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">PC Gaming</a>
+                            <br>
+                            <a href=""style="text-decoration: none;">Headphones</a>
+                        </ul>
+                </li>
+            </ul>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -65,17 +207,15 @@
                         <a class="nav-link  text-light" href="#">Contact</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-light" href="#">Log In</a>
+                        <a class="nav-link text-light" href="#">Sign Up</a>
                     </li>
                     <li class="nav-item dropdown   ml-lg-3 ">
                         <a class="nav-link dropdown-toggle text-light d-inline-block bg-orange rounded p-1 p-lg-2" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
-                            Sign Up
+                            Login
                         </a>
                         <ul class="dropdown-menu dropdown-menu-lg-right p-5 sign__container" aria-labelledby="navbarDropdownMenuLink">
-
-
-                            <h1 class="h4 mb-3 font-weight-normal text-center">Please sign in</h1>
-                            <label for="inputEmail" class="sr-only">Email address</label>
+                            <h1 class="h4 mb-3 font-weight-normal text-center">Login</h1>
+                            <label for="inputEmail" class="sr-only">Email Address</label>
                             <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
                             <label for="inputPassword" class="sr-only ">Password</label>
                             <input type="password" id="inputPassword" class="form-control mb-2" placeholder="Password" required="">
@@ -85,65 +225,61 @@
                                 </label>
                             </div>
                             <button class="btn btn-lg bg-orange btn-block text-light" type="submit">Sign in</button>
-
-
                         </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Basket</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
-
     <main class="container-fluid cover  ">
-
         <div class="row align-items-center justify-content-center justify-content-xl-start vh-80">
             <div class="col col-sm-7 text-center text-xl-left col-xl-4 offset-xl-2">
                 <h1 class="mb-4">Want some toys for your kids, gifts for your friend's child, or whatever. Let's buy it in our store.</h1>
                 <p class="mb-5 subtitle-mb">Games & puzzles, lego, baby & toddlers... We have it all.</p>
-                <div class="input-group mb-3 shadow">
-
-                    <input type="text" class="form-control" placeholder="Your toys..." aria-label="Search" aria-describedby="basic-addon1">
-                    <div class="input-group-prepend">
-                        <button class="btn bg-orange text-light text-uppercase p-3" type="button">Find It</button>
-                    </div>
+                <div class="box">
+                    <form class="sbox" action="/search" method="get">
+                        <input class="stext" type="text" name="q" placeholder="Tìm kiếm bài viết...">
+                        <a class="sbutton" type="submit" href="javascript:void(0);">
+                        <i class="fa fa-search"></i>
+                        </a>
+                    </form>
                 </div>
             </div>
         </div>
     </main>
-    <div class="product">
-    	<p style="text-align: left; font-family: arial; color: black; font-size: 30px; text-align: center; margin-top: 50px;">All Products</p>
-			<div>
-				<?php
-				$connect = mysqli_connect('3.132.234.157','doubleTToys','doubleT','doublettoysdata');
-				if($connect){
-					echo "";
-				}
-				else{
-					echo"Connection failed";
-				}
-				$sql="SELECT * FROM product";
-				$result=mysqli_query($connect,$sql);
-					while($row=mysqli_fetch_array($result)){
-				?>
+    <div>
+        <p style="text-align: left; font-family: arial; color: black; font-size: 30px; text-align: center; margin-top: 50px;">All Products</p>
+            <div>
+                <?php
+                $connect = mysqli_connect('3.132.234.157','doubleTToys','doubleT','doublettoysdata');
+                if($connect){
+                    echo "";
+                }
+                else{
+                    echo"Connection failed";
+                }
+                $sql="SELECT * FROM product";
+                $result=mysqli_query($connect,$sql);
+                    while($row=mysqli_fetch_array($result)){
+                ?>
                <div class="product">
                     <a>?id=<?php echo $row['product_id']; ?>" target="_blank"><img src="img/<?php echo $row['product_image'];?>" width="270px" height="270px"></a>
                     <p><?php echo $row['product_name'];?></p>
                     <p style="font-size: 13px"><?php echo $row['product_price'];?></p>
                </div>
             </div>
-				<?php
-				}
-				?>
+                <?php
+                }
+                ?>
     </div>
-
     <footer class="page-footer font-small bg-light">
-
         <div>
             <div class="container-fluid">
-
                 <!-- Grid row-->
                 <div class="row py-4 d-flex align-items-center">
-
                     <!-- Grid column -->
                     <div class="col-md-7 col-lg-5 col-xl-4 text-center text-md-left  mb-md-0 small">
                         <nav class="nav justify-content-center justify-content-md-start">
@@ -158,8 +294,6 @@
                         <div class="footer-copyright text-center text-black-50 pt-3 pb-2">© 2020
                         </div>
                     </div>
-
-
                     <!-- Grid column -->
                     <div class="col-md-2 col-lg-3 col-xl-2 text-center text-md-right">
                         <a href="#" class="text-dark">
@@ -168,18 +302,14 @@
                             <i class="fab fa-twitter white-text mx-2"> </i></a>
                         <a href="#" class="text-dark">
                             <i class="fab fa-instagram white-text mx-2"> </i></a>
-
-
-
                     </div>
                     <!-- Grid column -->
-
                 </div>
                 <!-- Grid row-->
-
             </div>
         </div>
     </footer>
 </body>
 </head>
+</html>
 </html>
